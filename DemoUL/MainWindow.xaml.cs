60c28bf53e5dc58b460d105cc2ed93ec503de415
student <student@K21-02-STUDENT>
2023-04-04 17:07:31 +0500

    04.04

--- DemoUL/MainWindow.xaml.cs
+++ DemoUL/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,48 @@ namespace DemoUL
             InitializeComponent();
 
             connection = new user2Entities();
+
+            BindingLvProduct();
+        }
+
+        public void BindingLvProduct()
+        {
+            ObservableCollection<Product> products = new ObservableCollection<Product>(connection.Products.ToList());
+
+            LvProduct.SetBinding(ItemsControl.ItemsSourceProperty, new Binding()
+            {
+                Source = products
+            });
+
+        }
+
+        private void Filter()
+        {
+            string searchString = Search.Text.Trim();
+           // string category = "";
+
+            var view = CollectionViewSource.GetDefaultView(LvProduct.ItemsSource);
+            view.Filter = new Predicate<object>(o =>
+            {
+                Product product = o as Product;
+                if (product == null) { return false; }
+
+                bool isVisible = true;
+                if (searchString.Length > 0)
+                {
+                    isVisible = product.Name.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) != -1 ||
+                        product.Description.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) != -1;
+                }
+
+                //isVisible = isVisible && category == product.Category;
+
+                return isVisible;
+            });
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
         }
     }
 }
