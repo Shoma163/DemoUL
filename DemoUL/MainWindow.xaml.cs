d6ab81248d1aeb50984c323965add9f36d8855c2
student <student@K21-02-STUDENT>
2023-04-04 17:08:11 +0500

    04.04

--- DemoUL/MainWindow.xaml.cs
+++ DemoUL/MainWindow.xaml.cs
@@ -16,9 +16,6 @@ using System.Windows.Shapes;
 
 namespace DemoUL
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private user2Entities connection;
